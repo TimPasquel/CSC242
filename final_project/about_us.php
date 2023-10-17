777a5624046f0fdc7ae1083436d7d4c7a30fa50f
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2023-10-17 18:49:54 -0400

    Create about_us.php

---
+++ final_project/about_us.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>About Us</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php include "nav.html" ?>
+<h1>About Us</h1>
+<h2>About the Developer</h2>
+<p>The Developer is a Student at Kutztown University who decided to make his CSC242 Final Project
+a way to help car enthusiasts find an create events.</p>
+<p></p>
+<p>While he is only a second year student, he hopes that his website can prove to be useful to other enthusiasts.</p>
+<h2>About the Organization</h2>
+<p>As stated above, ReadySetRace is an organization whose goal is to help car enthusiasts find an create events.</p>
+<p>Therefore, the website has a unique log in function with individual accounts, a local events page that lists all of the local events, </p>
+<p>a create event page for users to create their own events, a delete event page for users to delete their events, an update event time page </p>
+<p> so that users can update their event time, and a search events page so that users can search for specific events based upont their selection. </p>
+<img src="images/20210815_120147.jpg" alt="Twin 240SX" style="width:400px;height:500px;">
+</body>
+</html>
